7e2558e3ddcbe58c04aae6564dd8e51523b568c7
Rituraj Roy <riturajroy2927@gmail.com>
2025-10-19 12:30:45 +0530

    Schemas CREATED

--- Backend/models/userSchema.js
+++ Backend/models/userSchema.js
@@ -1,5 +1,46 @@
 const mongoose = require('mongoose');
 
-const connectDB = async()=>{
-    mongoose.connect({})
+const userSchema = new mongoose.Schema({
+    fullName:{
+        firstName:{
+            type:String,
+            required:true,
+            minlength:[3,"First name must be of 3 characters"]
+        },
+        firstName:{
+            type:String,
+            required:true,
+            minlength:[3,"First name must be of 3 characters"]
+        },
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        minlength:[5,'Email must be 5 characters long']
+    },
+    password:{
+        type:String,
+        required:true,
+        select:false
+    },
+    socketId:{
+        type:String
+    },
+})
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET);
+    return token;
 }
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.statics.hashPassword = async function (password){
+    return await bcrypt.hash(password,10);
+}
+
+
+const userModel = mongoose.model('user',userSchema)
+model.exports = userModel
